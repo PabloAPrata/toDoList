d314d4556f1a52a8909c5741cbc715b4922b6344
Pablo Almeida <pabloalmeidaprata@hotmail.com>
2022-10-02 23:18:18 -0300

    Removendo os consoles.logs

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Chamando as dependencias e gravando os pacotes em constantes para trabalhar com eles no Javascript
 const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+// const morgan = require('morgan');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
 const routes = require('./config/routes')
 
 // Iniciando o servidor
 
 const app = express()
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(morgan('dev'));
+// app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(routes);
 app.use(express.static('public'));
 
